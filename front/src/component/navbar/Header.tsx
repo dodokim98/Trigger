188ffe25493667794ff9e6b2715ecb83d2723c38
kennedy1234 <kennedy1234@naver.com>
2024-05-03 11:15:56 +0900

    test: jenkins3
    Former-commit-id: 39cbf38f65a46414233c74bdaedc83d851614ca3

--- front/src/component/navbar/Header.tsx
+++ front/src/component/navbar/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c977770467152af5af0ef065613f9dbdeefa8b053930f4c20176783c1e251576
-size 1622
+oid sha256:be841506b4be694b07aff574fc6b9127a57e517beb127b2a19d10a89d4da3152
+size 1554
