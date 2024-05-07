15177a986b36ff1b1931dd512c391af2b595da92
kennedy1234 <kennedy1234@naver.com>
2024-05-07 11:11:52 +0900

    test: git pull origin develop_be
    Former-commit-id: 4a496f61c9f0998009428822b4c83673be69c273

---
+++ back/src/main/java/com/ssafy/c109/trigger/domain/room/entity/Room.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:318a82b7d65fc42dc0256b9d8f43ac6311840b72cd24d439d4c96de8d9ac8149
+size 831
---
+++ back/src/main/java/com/ssafy/c109/trigger/domain/room/service/RoomServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e37232bada4f14a8220740a09d93fb427b475d5386c76c1dbca83b1ca934025
+size 1598
