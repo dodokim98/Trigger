275d2fc3c1b1afc063b4ba74c2efed0419454725
dodokim98 <eh6848zzz@gmail.com>
2024-05-08 02:42:26 +0900

    fix : #485 공지사항 상세글 보기 api
    Former-commit-id: d322cdc72ea186de0d600802168d9a8b0e2133c7

--- back/src/main/java/com/ssafy/c109/trigger/domain/notice/controller/NoticeController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/notice/controller/NoticeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4ea54271a834b45c8f8c983a0cd4371f3cb15bfcd0fdd70c2cba600539aa993
-size 1879
+oid sha256:15b5f83dc296d29cd167e2f73204f9becb83a6fa7c3f4351b55beeb813d92332
+size 1904
