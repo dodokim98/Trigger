bf8ba590738776d40f19a89a3402dbc8343080a7
dodokim98 <eh6848zzz@gmail.com>
2024-05-07 12:12:02 +0900

    feat : 유저 회원가입 기능
    Former-commit-id: 8a0d9f70b75b1835b462a2ebaa99d6eb79e789f6

--- back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02fba79ca709fa7d8b5d5795bf04df2e32d417b660c7a04b98384fd59f67e924
-size 1099
+oid sha256:bbfc47609ec92672593216049207eb8c7fa6601378db93cdd35e57330440a4ee
+size 1202
