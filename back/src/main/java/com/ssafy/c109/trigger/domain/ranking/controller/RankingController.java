7b0595f615a38478049d234e0624e6e595074411
dodokim98 <eh6848zzz@gmail.com>
2024-05-08 16:48:41 +0900

    feat/communityfiles
    Former-commit-id: 5da2a9c2c8834b7d686adfb51700307216be8fcb

--- back/src/main/java/com/ssafy/c109/trigger/domain/ranking/controller/RankingController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/ranking/controller/RankingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82d7786f613427b9f1948194d876713100402fe6b6a0b45eb66ea912bbb68937
-size 1224
+oid sha256:0560c91abfbb69d91ad5bd4aca0eec6f89dcde0ea500ba2436d689228d5e9e5f
+size 1226
