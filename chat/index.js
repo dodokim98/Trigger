5d5a462d9963da5abf20c952432654ec1cf47b23
unknown <yhyh328@naver.com>
2024-05-08 11:38:26 +0900

    fix: chat/Dockerfile cors_origin localhost:3000
    Former-commit-id: c8415532cef2b77d52e6d86b91acefa1bd83e925

--- chat/index.js
+++ chat/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a118cba55884d235bf747fe716f281b25f16172c968ec9002a4bfb903cce4235
-size 868
+oid sha256:af4a685e3f7608395dd54d0421d2292a40cd11d7d230144863e153693ccdd576
+size 889
