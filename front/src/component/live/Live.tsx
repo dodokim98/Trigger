d7b072ccfb31e66b28a71b73555db30fb9d89570
dodokim98 <eh6848zzz@gmail.com>
2024-05-09 11:48:00 +0900

    test : front/Dockerfile
    Former-commit-id: bf11e8497c81359a545a49117376dad1ca86fa42

--- front/src/component/live/Live.tsx
+++ front/src/component/live/Live.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a30eda5435fe713114510d7d7c7df07d37172eb926a5f9859d56910625dbe9fd
-size 1262
+oid sha256:8b24e98929bec67aec289086ffe71a7c48e4fa1fbc882c96a225c216fe446467
+size 1321
