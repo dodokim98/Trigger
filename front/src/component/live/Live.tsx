a6821cd3ed8b81da12f6a633d562f2a619102da5
dodokim98 <eh6848zzz@gmail.com>
2024-05-09 12:08:14 +0900

    test : front/server.js
    Former-commit-id: d3e00acd2c70e866e02d2b6c01d1500cddd0ee18

--- front/src/component/live/Live.tsx
+++ front/src/component/live/Live.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b24e98929bec67aec289086ffe71a7c48e4fa1fbc882c96a225c216fe446467
+oid sha256:bcf28a060db9fd9a55177650b2e1ab2153cfdce84b55103211f51fb5222a8c44
 size 1321
