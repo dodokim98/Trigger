78ac2ee3f649f89f85b285a2c52d2468de6eeed1
unknown <yhyh328@naver.com>
2024-05-10 17:32:13 +0900

    feat: front/src/api/notifications.ts & front/src/util/
    Former-commit-id: 537322bee97c41055a8485e1ea27534759d2a7f4

--- front/src/api/notifications.ts
+++ front/src/api/notifications.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7016f070209808c021a4173d87a385b17a5bd0deafc7732c9f3f8da3a9349310
-size 2232
+oid sha256:50961cc3545fa4f1a84b82e65f5344e65773ed2b1b4b0e92ccd4cb78a6374676
+size 2208
--- front/src/util/http-commons.ts
+++ front/src/util/http-commons.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b920ef3bcd195fab49c745cad3a171cbdd9c23601e3f55ea1f4d9b6d500f9de
-size 618
+oid sha256:81aa8532659f5f6a0ec4f8a287da7f583d9a681e211cf30daaa664da16b0600c
+size 856
