d781512cc996ec81b258f97cb84527ace909dd0e
dodokim98 <eh6848zzz@gmail.com>
2024-05-10 14:51:01 +0900

    test : front/src/LiceDetail.tsx log
    Former-commit-id: a484549423ace8bc31d943c25b05b4eac581a2a4

--- front/src/component/live/LiveDetail.tsx
+++ front/src/component/live/LiveDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7d16292c0b17cae0d2471024c219c7c68cb0bc7f2cb0f2d0395ccf2f3ed2a93
-size 889
+oid sha256:3d201f76bdb27fd6009c6ee5b93f86403c922fe91ec5473eead6af749bf2544c
+size 827
