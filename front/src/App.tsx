9171d34fae5250bfec9b45234cfa2cbfc84d5191
kennedy1234 <kennedy1234@naver.com>
2024-05-10 17:24:49 +0900

    feat: signup form
    Former-commit-id: b2907ca1ec35c7031e36b33d1e759f3e8ad3192c

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b1b460792262c9a7433b8c239b45eea1e06064f73eb90a457a61e2584566f68
-size 1472
+oid sha256:d9cc134e1da6952a2c8118aa5eba6072bd82e53c916dca245e03480b9a095df2
+size 1573
---
+++ front/src/component/member/SignUp.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:042ec746c2f48f65b43314e6671e069f9ad04e3c4d83c13c4328a104bbe23c10
+size 1857
