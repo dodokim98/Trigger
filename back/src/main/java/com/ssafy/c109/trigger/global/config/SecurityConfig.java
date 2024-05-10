3257df622577c45d9b28f3d64baed2319ff20ea2
dodokim98 <eh6848zzz@gmail.com>
2024-05-10 15:53:21 +0900

    fix : back - cors
    Former-commit-id: 5bca91e05de08f23de985deadab49bb8a06f1527

--- back/src/main/java/com/ssafy/c109/trigger/global/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/c109/trigger/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca8429871b42d2817e567caaf47bd04ce91364e5c75192513271bf2d2b334911
-size 6644
+oid sha256:6ac1f70efa89f7dd0ae9f2bd1be0b0e58eb4d3a649a715283eaa6d294dc27057
+size 7673
