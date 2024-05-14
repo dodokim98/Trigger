0a14b0322cdc9a7542194964a990f6db4d7ebd63
dodokim98 <eh6848zzz@gmail.com>
2024-05-14 15:22:49 +0900

    test : 유저 프로필 업로드,
    Former-commit-id: 243ec455d4cc9143db0597e538d7501360b0de24

--- back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f17bceb9d3bacd3ec8cc348f614e2bceade912db81945e9a7fe998b9b4b1a63d
-size 1315
+oid sha256:66dcb08d895353e39a7c643de62aa5c74a8d83c4e1c867724119d16c8e6c8841
+size 1363
--- back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/OauthController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/OauthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0e96b68f9abd5a89f158e025bdf01ce39fc79ecd279da50ecb0d93917eb3260
-size 1594
+oid sha256:dc3d0debb7e419c29a65c9d4ee1ce3950bf427d37cba3965948a8075c271b7de
+size 1699
