acc681564ac6f0546a462af7e76e6f1444b3ba88
unknown <yhyh328@naver.com>
2024-05-14 15:03:57 +0900

    refactor: public/index.html
    Former-commit-id: 7f11326de92b2d8a859a9d568af09d0050757f00

--- front/src/component/main/MainSection2.tsx
+++ front/src/component/main/MainSection2.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3484ae291ebe2bf054c38344fd35ddf2a37656d306670d69837aac2c1d36c2d5
-size 4528
+oid sha256:8bf8b3ef6d6e3a9ad2472540ebe186296b53e4d303c63e647e30d53cd8ead1a6
+size 4609
