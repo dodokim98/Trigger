4b2cc10fd3ee2ac84e1371d43cfb0eb42a96d897
kennedy1234 <kennedy1234@naver.com>
2024-05-14 17:33:50 +0900

    test: Member Api Test
    Former-commit-id: ca62064293d3e0f6034144b95d9ae457b39041ce

--- front/src/component/member/SignUp.tsx
+++ front/src/component/member/SignUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:711c6a92251fb1e1dbbb83855c87599a042c4d387d627bd96604d34a988969ba
+oid sha256:84b877ae3774f8e037464cabeae8c0e31736933cff26af7d14abd60601f58c20
 size 4310
