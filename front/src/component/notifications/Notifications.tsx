915e998dc9021b4c5514ed732829cc5ed3f045de
hyein <haeinsuin@naver.com>
2024-05-14 14:24:10 +0900

    fix fe
    Former-commit-id: b9b1d5c255e82afc4cd75f1e22347507c05a943c

--- front/src/component/notifications/Notifications.tsx
+++ front/src/component/notifications/Notifications.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c89bcfa14f9a5d354a6518baf33e13fdda0e7dc8cfc4953ec920203d44f6495
-size 1784
+oid sha256:39ba8664e8b00129a6a83932ea42741c4415709984974e8d92507fe299ccaac5
+size 1783
