6a50ad920f73872d51f026f53fc558733997f86f
dodokim98 <eh6848zzz@gmail.com>
2024-05-14 17:23:22 +0900

    test : user 등록
    Former-commit-id: a052133d4b36e407ba24759be4c46b771381f693

--- back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1260669dec3f1bf42f3bdd4535b0addbb0da6c878d03647e1ebb7c434212cf0a
-size 1364
+oid sha256:4b556fe8a0de792a44943209b3bedfee7d4e919f4cf7d3410359eb35fc3917ca
+size 1382
