89a5769e2066b5e7e56112ea7031bd5a58a4a5d3
hyein <haeinsuin@naver.com>
2024-05-14 15:03:27 +0900

    fix fe
    Former-commit-id: 3decc5b50d870d62ff203b98c45e1fa06ddaa501

--- front/src/component/main/MainSection2.tsx
+++ front/src/component/main/MainSection2.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3484ae291ebe2bf054c38344fd35ddf2a37656d306670d69837aac2c1d36c2d5
-size 4528
+oid sha256:33ee8776357419e703b19ee28bf8a91a270edea6c66d9c8e60187cb0f68e4735
+size 3820
