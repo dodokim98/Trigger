b2d70b4651b039fa7731c907aea41dfbc530edc1
dodokim98 <eh6848zzz@gmail.com>
2024-05-14 10:44:45 +0900

    test : notice 기본 프로파일 이미지
    Former-commit-id: 51c3fcb07f9253799322549cabf4d27077c15279

--- back/src/main/java/com/ssafy/c109/trigger/domain/notice/service/NoticeServiceImpl.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/notice/service/NoticeServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fd9e4d6020f2620469fb5ea5db420e62e33704038d6cd6656c0f96581e30487
-size 4679
+oid sha256:e1f4189315fed32635565f9c5b19b195f6fb0d0ba85dc2dfc7371defa9db291d
+size 4733
