ab3eabf1e67686927b92a60565391760a3140b99
dodokim98 <eh6848zzz@gmail.com>
2024-05-16 15:14:29 +0900

    test : room API security Test
    Former-commit-id: b87ff40bbe12cdfa34c93baf7d23db42db90bd55

--- back/src/main/java/com/ssafy/c109/trigger/global/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ back/src/main/java/com/ssafy/c109/trigger/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e6a316da630705d66ba79510465c817f0b4a6b04ef2af93f10d99998176ec55
-size 9256
+oid sha256:ebe6588a2387712fa9a72c10392cc474674af2c11ad97256bd1d9f16ca3c48a8
+size 9426
