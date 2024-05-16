83d6993d2ab8e8e8b28ea04ba99d479fd182b7d7
dodokim98 <eh6848zzz@gmail.com>
2024-05-16 13:55:19 +0900

    fix : room 생성 API
    Former-commit-id: c3e91f524694bce19c8ba61df7a01424338c9685

--- back/src/main/java/com/ssafy/c109/trigger/domain/room/controller/RoomController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43635c1042f062d215de25797ddce5b52a2b8ce85b1dd05cd7a490d2c8f2c9b6
-size 1474
+oid sha256:3a7d6aca2785b8ed0df29a33c9cde6d914c8194ae9933c3753b235361c1a4912
+size 1475
--- back/src/main/java/com/ssafy/c109/trigger/global/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/c109/trigger/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:240a3f3f025667e6b28a5ea329441cdcd0d57f6bfaf57078505b53fbf276c795
-size 8018
+oid sha256:9d638b0cab971f368ad6e7f63dc580441f585b562099f479eaf657733b82ba6b
+size 8090
