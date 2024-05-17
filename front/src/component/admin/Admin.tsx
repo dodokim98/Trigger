3ad4e0cda1d933685495fabb1c018fd18dd21159
unknown <yhyh328@naver.com>
2024-05-17 17:14:05 +0900

    fix: package.json
    Former-commit-id: c74a6cc62b09de88fa1a812d1f17f5a5f4477b84

--- front/src/component/admin/Admin.tsx
+++ front/src/component/admin/Admin.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43c7e3e4f984dde33ec6a22c2276826852a009ff6ee0e30516deb1df16f0952c
-size 3797
+oid sha256:1c2355ac756e7492bc687370db9a1f89399a9e268294da0321323361b2063ae1
+size 4017
