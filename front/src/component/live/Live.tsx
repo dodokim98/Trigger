9820f17ad8bed2fa2ecbe3e909ec996fd91da2d8
kennedy1234 <kennedy1234@naver.com>
2024-05-17 11:24:45 +0900

    feat: roomTitle
    Former-commit-id: 37887c9c7f2b2ba323f018c462cfd3de3a19e099

--- front/src/component/live/Live.tsx
+++ front/src/component/live/Live.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81dad1d0dd35a70a8d84074a37dfe4808935bdbe677c656988315e426bd500e9
-size 1894
+oid sha256:5047edc96ecac5c2190f07da4d1b30cd307e7ce2128065c6c12ec51d593c2cbe
+size 2546
