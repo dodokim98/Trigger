ed0c07528cb0feb84c999f24b8a8677d1329d879
unknown <yhyh328@naver.com>
2024-05-17 10:49:50 +0900

    test: notifications.ts
    Former-commit-id: c9008c3d52b95ee53e8f1644799075afbb43eb4d

--- front/src/api/notifications.ts
+++ front/src/api/notifications.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73f425bb9be865b8283b771b8818e3729e6193e871c378b2cb7f7addb2e76b02
-size 4300
+oid sha256:c5b691eed6b896407e2ac77ab0b0caebd511813e3b18bf74019971b65e9216fb
+size 4417
