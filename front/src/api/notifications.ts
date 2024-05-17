cfd266c11f948b15503012c0cebd4437c37ddf95
unknown <yhyh328@naver.com>
2024-05-17 14:35:27 +0900

    fix: front/src/api/notifications.ts
    Former-commit-id: 97f5dcad35369a8bfe25b37f39bb1cff03697490

--- front/src/api/notifications.ts
+++ front/src/api/notifications.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e83251f0eab291889707a691d5401c7218c5fd5b4d71a9a9a30f79dbc65c0d9f
-size 3945
+oid sha256:65b27e7b664dd23e0826315ddd702190ce0e4a6c8665eb47b411307bd5db7489
+size 3970
