29b768968bc2fb9799a6a278245133292fdead98
dodokim98 <eh6848zzz@gmail.com>
2024-05-17 16:40:39 +0900

    test : 공지사항 이미지 test
    Former-commit-id: 478502207f66f1e71ad54bc4c9cf79a4799f7230

--- back/src/main/java/com/ssafy/c109/trigger/domain/notice/controller/NoticeController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/notice/controller/NoticeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a92e03cbd1c49077c299b529990e19d9f3858e5787204cba5a5a30f01e3fb973
-size 3370
+oid sha256:0948914aa87db1a9fbc880acaae051d4b0fb761dd522648bbe468eaa76d84d6f
+size 3416
