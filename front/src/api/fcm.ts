d564ce2f139785e90ef483cb40b8db3570aa264f
unknown <yhyh328@naver.com>
2024-05-17 15:18:33 +0900

    refactor: notification.ts
    Former-commit-id: 867294e58cf678d6e2a7eb09dd5517e7c83aaec1

--- front/src/api/fcm.ts
+++ front/src/api/fcm.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d68a2fd52e1e8da79d26188a080103c12eacc46de3e05dbe7a06b892f40323d
-size 1321
+oid sha256:3abe5f532e72c1fdcaa96442d360258731c9adbd45c059437688384add939c85
+size 1324
--- front/src/api/notifications.ts
+++ front/src/api/notifications.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65b27e7b664dd23e0826315ddd702190ce0e4a6c8665eb47b411307bd5db7489
-size 3970
+oid sha256:b0858f306c8ed112b7e6d01ddba1b95292bcd3afe52ae4c949314fbc78bf3030
+size 4053
