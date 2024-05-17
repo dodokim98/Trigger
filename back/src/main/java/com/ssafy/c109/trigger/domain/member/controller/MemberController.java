207bfaea995625600443e1b4311f3e235b3ab3a5
dodokim98 <eh6848zzz@gmail.com>
2024-05-18 02:19:22 +0900

    fix : signUp API
    Former-commit-id: 9e12bac239f40dae60a99491a2ede6d9e0038cff

--- back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3625ff7e8eb0330132570eb343cb978958c1774a9323aae77a832f7bad35304e
-size 2481
+oid sha256:c875dfa02d6fd9c94e709521a43a0a9c83d58a74272ef706af841b3e5eb89858
+size 2507
