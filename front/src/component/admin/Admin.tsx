966704e27348f3f5a2fa44dd6aa1dd43f2180523
unknown <yhyh328@naver.com>
2024-05-17 16:46:51 +0900

    fix: Admin.tsx
    Former-commit-id: 729aa7ed5a4786b2456879518799a5237eb78eaf

--- front/src/component/admin/Admin.tsx
+++ front/src/component/admin/Admin.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9eb16480c6ca7a6d4de92529e5a39e1e7f9e5d35dc83f9a2c482f148ed6b03b8
-size 3750
+oid sha256:43c7e3e4f984dde33ec6a22c2276826852a009ff6ee0e30516deb1df16f0952c
+size 3797
