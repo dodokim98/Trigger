cda338019f9c74a4b012737f7716bfcb50c78cd2
unknown <yhyh328@naver.com>
2024-05-18 18:41:44 +0900

    fix: notifications.ts
    Former-commit-id: f6ab9bd314a2a3b68184d7225b39e265afc2d10a

--- back/src/main/java/com/ssafy/c109/trigger/domain/notice/service/NoticeService.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/notice/service/NoticeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d355b7bedbb44315884cb1981c1b4fdc38428f87410e8733ea1a5a2254f387ee
-size 647
+oid sha256:138769aaa440ac67fa24e0bd29bfae08045c1b475f2f39892c01a660606718c7
+size 648
