6591379bb1f97f6dfe9752f0a13958713c144e4e
unknown <yhyh328@naver.com>
2024-05-18 23:52:46 +0900

    fix: Notifications.tsx
    Former-commit-id: 3ec2942e996c2cdc77ee7afd6fa66f5492f474bd

--- front/src/component/notifications/Notifications.tsx
+++ front/src/component/notifications/Notifications.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17ca24ed5f160ba8774e7426ae22793c1372da845fa8547b293742948bad2de5
-size 4631
+oid sha256:8807dbd8aebff3cd994103b37851d5711f21853c006cbb7cbb6a79941f63ae6d
+size 4571
