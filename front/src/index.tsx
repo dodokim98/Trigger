bf01e9073be5086654dacbb06ba95c11502d570b
unknown <yhyh328@naver.com>
2024-05-19 00:16:15 +0900

    fix: index.tsx
    Former-commit-id: 366a33276d96f048c148d6fd3360f9415ca80bec

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40a7aeb3b55a830bb62cd8dc82a0b6c3104eee000fca4098ff6b1b4251fe8607
-size 744
+oid sha256:12894d0561620f7262db82dac2be0b9365713e14778569d6288f954e216d3547
+size 750
