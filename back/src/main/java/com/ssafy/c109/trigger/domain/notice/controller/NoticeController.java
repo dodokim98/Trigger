76b9f00ca2ae94d0fb8dca245dccd9ee23b308eb
dodokim98 <eh6848zzz@gmail.com>
2024-05-18 19:13:56 +0900

    fix : postNotice API
    Former-commit-id: 6b058d0a8c718bf89111ef9f8b7056fcb878fbac

--- back/src/main/java/com/ssafy/c109/trigger/domain/notice/controller/NoticeController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/notice/controller/NoticeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67bd60dfb5d8e964fee8f33372d0cd65bc309e5e60773179f4b039748457d98d
-size 3833
+oid sha256:7c72304ea83ce83c2bc4297be71d841990a9bc1980e6b18885cc9b3a9d716fe6
+size 3840
