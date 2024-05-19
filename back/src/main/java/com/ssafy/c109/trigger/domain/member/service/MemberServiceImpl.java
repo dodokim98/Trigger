6171efccc037546bd15fa555995aafefdf2b73aa
dodokim98 <eh6848zzz@gmail.com>
2024-05-19 21:35:54 +0900

    fix : ranking list API
    Former-commit-id: 83aee432efb0039561a78229c0ff1a04ea73aab8

--- back/src/main/java/com/ssafy/c109/trigger/domain/member/service/MemberServiceImpl.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6395c63fcccca6ff603d6c9dc318e04ec631c446b362dd17ff737ae51755fda4
-size 3688
+oid sha256:da112dd23bf860a7b86aa9f256c1d21350fa3d576235aa7f0d11f3afe05af51e
+size 3700
