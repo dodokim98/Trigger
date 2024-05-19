3d408a6b04b5a5eca6767c5ead9b177aaedec63d
dodokim98 <eh6848zzz@gmail.com>
2024-05-20 04:50:27 +0900

    fix : trigger default logo
    Former-commit-id: 76988a2d21e70acda340783691c941fa2b67f23e

--- back/src/main/java/com/ssafy/c109/trigger/domain/member/service/MemberServiceImpl.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a28a8b3b37ae3b0905c9b554f8f9c7333d185f8fbf3df5a76c4346570245c42
-size 4574
+oid sha256:c3ca8b512f8752664a90d7c14fa0ee877cc9750c2ad7c42ae5b2bb944fdd4dc8
+size 4590
