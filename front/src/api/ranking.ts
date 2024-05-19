86d9c2fc7cec8c62a18cfd218f018c8b0d0e9941
unknown <yhyh328@naver.com>
2024-05-20 05:19:49 +0900

    test
    Former-commit-id: 7159c880e45e75b2f57c3010858b288c5791f226

--- front/src/api/ranking.ts
+++ front/src/api/ranking.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1aa8f182035a958990a2631f965ad08dd5b0cb8a3777346e6d275e36e13a5b5
-size 3142
+oid sha256:d429dbe249adde0e898877ed0b7ceeca3b6d5aa346920b4288afe90da19e2d54
+size 1158
