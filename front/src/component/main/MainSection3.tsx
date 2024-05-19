aeb38abd5ad6bd94ec9684fed57d3c606fcff314
unknown <yhyh328@naver.com>
2024-05-20 05:34:55 +0900

    fix: MainSection3
    Former-commit-id: 7d6473cdce74a51bb5b23cb1a156fd39f8bc9c69

--- front/src/component/main/MainSection3.tsx
+++ front/src/component/main/MainSection3.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98bdd31c365d89e2d1ca4bdec2ba474581f7f0b3c80aa0dd7b8f680d3e53d67f
-size 1122
+oid sha256:82339cdd66486f83f2740ba49e70bdce156652b65a07b8214dafb9345bbaac40
+size 1189
