e55c31c59d59f92fd755066d27cb28b5b96bc062
unknown <yhyh328@naver.com>
2024-05-20 09:33:17 +0900

    feat: 초보자가이드
    Former-commit-id: 0303406035fc4b1d7e69f0c387fb56adf589a573

--- front/src/component/guide/dummy-guideImg.ts
+++ front/src/component/guide/dummy-guideImg.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dafaeeced0bda2d4558f204c090d53808a7d8ef7d38b284838ca0576a0548d9e
-size 599
+oid sha256:de1d487b81eb40d9ed81dc6e0965ceba5a3b894da20a4ff9e2d8bc4657ba5fc3
+size 651
